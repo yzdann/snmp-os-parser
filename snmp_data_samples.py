d301aa40ab35cd571fab8f090e3bb5fe2939ae7b
yzdann <1543472-yzdann@users.noreply.gitlab.com>
2019-12-03 13:31:05 +0330

    add python file that contain list of snmp samples output

---
+++ snmp_data_samples.py
@@ -0,0 +1,8 @@
+snmp_samples_list = [
+    "Linux ubuntu-bionic 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64",
+    "Linux ubuntu-xenial 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64",
+    "Linux vagrant-ubuntu-trusty-64 3.13.0-170-generic #220-Ubuntu SMP Thu May 9 12:40:49 UTC 2019 x86_64",
+    "Linux localhost.localdomain 3.10.0-957.12.2.el7.x86_64 #1 SMP Tue May 14 21:24:32 UTC 2019 x86_64",
+    "Linux centos6.localdomain 2.6.32-754.24.3.el6.x86_64 #1 SMP Thu Nov 14 15:35:16 UTC 2019 x86_64",
+    "Linux localhost.localdomain 2.6.32-696.el6.x86_64 #1 SMP Tue Mar 21 19:29:05 UTC 2017 x86_64",
+]
